5bc7c948c8ead8aa2db4d5ea84f4ab9c5c648aaa
PeterRMay <prmaywa@gmail.com>
2024-02-28 16:06:30 -0800

    added saturation

--- servoCalc.m
+++ servoCalc.m
@@ -1,8 +1,17 @@
-function [alpha] = servoCalc(P,B,l,s,a,beta)
+function [alpha] = servoCalc(P_base_i,B_i,l,s,a,beta)
+    
     L = l^2 - (s^2 - a^2);
-    M = 2*a*(P(3)-B(3));
-    N = 2*a*(cosd(beta)*(P(1)-B(1)) + sind(beta)*(P(2)-B(2)));
-    alpha = asind(L / (sqrt(M^2 + N^2))) - atand(N/M);
+    M = 2*a*(P_base_i(3)-B_i(3));
+    N = 2*a*(cosd(beta)*(P_base_i(1)-B_i(1)) + sind(beta)*(P_base_i(2)-B_i(2)));
+    foo = (L / (sqrt(M^2 + N^2)));
+    if foo > 1
+        foo = 1;
+        disp("limits exceeded")
+    elseif foo < -1
+        foo = -1;
+        disp("limits exceeded")
+    end
+    alpha = asind(foo) - atand(N/M);
     if isnan(alpha)
         alpha = 0;
     end
