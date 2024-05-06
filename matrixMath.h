22eb1070a2f122e2b1bc9d3e2658c9032cc2af10
JoeAsav401 <joeasavalahaphun@gmail.com>
2024-05-06 14:48:04 -0700

    updated matrixMath

--- matrixMath.h
+++ matrixMath.h
@@ -9,8 +9,9 @@ void rotZ(double angles[3], double R[3][3]);
 void rotZYX(double angles[3], double R[3][3]);
 void vectorAdd(double A[3], double B[3], double result[3]);
 void vectorSubtract(double A[3], double B[3], double result[3]);
-void matrixMultiply(double R[3][3], double v[3], double result[3]);
-void matrixMultiply2(double A[3][3], double B[3][3], double result[3][3]);
+void matrixMultiply31x31(double v1[3], double v2[3], double result[3]);
+void matrixMultiply33x31(double R[3][3], double v[3], double result[3]);
+void matrixMultiply33x33(double A[3][3], double B[3][3], double result[3][3]);
 void vectorScalarMultiply(double v[3], double scalar, double result[3]);
 
 #endif
